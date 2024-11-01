6e6e47fe5c7cbd531f39e5a8e3a94a8e1a2ac8f6
swopsyapp <swopsyapp@gmail.com>
2024-11-01 15:11:25 +0000

    Remove lucide-svelte as a direct dependency

--- src/lib/components/ui/ThemeToggle.svelte
+++ src/lib/components/ui/ThemeToggle.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-	import Sun from 'lucide-svelte/icons/sun';
-	import Moon from 'lucide-svelte/icons/moon';
+    import Icon from '@iconify/svelte';
 
-	import { toggleMode } from 'mode-watcher';
-	import { Button } from '$lib/components/ui/button/index.js';
+    import { toggleMode } from 'mode-watcher';
+    import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
 <Button on:click={toggleMode} class="w-1/2 justify-start">
-	<Sun
-		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-	/>
-	<Moon
-		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-	/>
-	<span>&nbsp;&nbsp;Theme</span>
+    <Icon
+        icon="lucide:sun"
+        class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
+    <Icon
+        icon="lucide:moon"
+        class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+    <span>&nbsp;&nbsp;Theme</span>
 </Button>
