a90069bc248aa88b6bf35d1df4265486f2d7766e
crowne <crow.neil@gmail.com>
2024-11-17 00:48:00 +0000

    Fixed ThemeToggle

--- src/lib/components/ui/ThemeToggle.svelte
+++ src/lib/components/ui/ThemeToggle.svelte
@@ -5,7 +5,7 @@
     import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<Button on:click={toggleMode} class="w-1/2 justify-start">
+<Button onclick={toggleMode} class="w-1/2 justify-start">
     <Icon
         icon="lucide:sun"
         class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
